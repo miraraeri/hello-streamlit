9d32ce8bacf9c4ea00d2ce634122debce91da142
miraraeri <155308644+miraraeri@users.noreply.github.com>
2024-01-13 20:45:28 +0000

    Добавлены заголовки и плитки

--- Hello.py
+++ Hello.py
@@ -3,4 +3,13 @@ from streamlit.logger import get_logger
 
 LOGGER = get_logger(__name__)
 
-st.title('Это название')
+name = st.title('Это название.')
+st.header('Это заголовок.')
+st.subheader('А это подзаголовок.')
+st.text('Здесь можно писать текс. Например, "Привет".')
+st.markdown("This is a markdown") #не поняла эту функцию
+st.success('Плитка успеха (зеленая)')
+st.info('Плитка информации (синяя)')
+st.warning('Плитка предупреждения (желтая)')
+st.error('Плитка ошибка (красная)')
+st.info("пример")
